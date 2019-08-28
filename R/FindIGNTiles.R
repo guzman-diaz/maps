a10fdd5368d72a3a70f0628c69f9088dde884a81
good.man <guzman@uniovi.es>
2019-08-28 08:50:00 +0200

    Initial commit

---
+++ R/FindIGNTiles.R
@@ -0,0 +1,33 @@
+FindIGNTiles <- function(pointTable, is.lonLat = TRUE){
+  
+  # Load corner table
+  cornerTable <- readRDS(here::here('data', 'cornerData.rds')) %>% 
+    dplyr::filter(UTMzone == 30)
+  
+  # Iterate over all points
+  tileSet <- vector()
+  for (pointId in 1:nrow(pointTable)){
+    
+    ## Retrieve point coord
+    pointCoord <- pointTable[pointId, ] 
+    
+    ## Convert to xy if lonlat
+    if (is.lonLat){
+      pointCoord <- TransformCoordinates(pointCoord, is.lonLat = T)
+    }
+    
+    ## Find tile
+    tileSet <- cornerTable %>% 
+      ### Filter tiles containing the point
+      dplyr::filter(xmin <= pointCoord$x & pointCoord$x <= xmax &
+                      ymin <= pointCoord$y & pointCoord$y <= ymax
+      ) %>% 
+      ### Select the tile ids
+      dplyr::select(name) %>% 
+      ### Combine the result with that of previous points
+      union(tileSet) %>% 
+      unlist()
+  }
+  
+  return(tileSet)
+}
