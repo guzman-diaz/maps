e9fa09d27d6fec9de976e2b0ca44e5dd75c9b202
good.man <guzman@uniovi.es>
2019-08-29 17:08:19 +0200

    Initial commit

--- R/CropRaster.R
+++ R/CropRaster.R
@@ -1,21 +1,5 @@
 CropRaster <- function(rasterObject, boundingBox, zoomLevel = 1){
   
-  # Check in xy coordinates are present
-  if (is.null(boundingBox$p1$x)){
-    boundingBox$p1 <-  c(boundingBox$p1, TransformCoordinates(as.vector(boundingBox$p1), is.lonLat = T))
-  }
-  
-  if (is.null(boundingBox$p2$x)){
-    boundingBox$p2 <-  c(boundingBox$p2, TransformCoordinates(as.vector(boundingBox$p2), is.lonLat = T))
-  }
-  
-  # Apply zoom
-  boundingBoxDiagonal <- 
-  
-  boundingBox <- with(boundingBox,
-                      p1$x <- p1$x
-  )
-  
   # Define polygon
   cropPolygon <- with(boundingBox,
                       as(raster::extent(list(x = c(p1$x, p2$x), y = c(p1$y, p2$y))), 
