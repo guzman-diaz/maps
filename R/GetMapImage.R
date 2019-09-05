3ca9c520a86266312191814f0030a27649e6d5a6
good.man <guzman@uniovi.es>
2019-09-05 16:59:36 +0200

    OSM method

--- R/GetMapImage.R
+++ R/GetMapImage.R
@@ -1,81 +1,46 @@
 GetMapImage <- function(boundingBox,
-                        majorDim = 600,
-                        mapType = c('World_Topo_Maps', 'World_Street_Map', 'World_Imagery')[1],
+                        mapType = 'esri-topo',
+                        majorDim = 4000,
                         imageFileName = here::here('figs', 'overlay.png')
 ){
   
-  GetArcGISMapImage <- function(boundingBox, mapType = "World_Street_Map", file = NULL, 
-                                width = 400, height = 400, sr_bbox = 4326) {
-    require(httr)
-    require(glue) 
-    require(jsonlite)
+  pacman::p_load(OpenStreetMap)
+  
+  # Map types
+  if (is.numeric(mapType)){
+    mapType <- c('osm', 'bing', 'stamen-toner', 
+                 'maptoolkit-topo',
+                 'waze', 'mapquest', 'mapquest-aerial',
+                 'stamen-terrain',
+                 'stamen-watercolor', 'osm-german', 'osm-wanderreitkarte',
+                 'mapbox', 'esri', 'esri-topo',
+                 'nps', 'apple-iphoto', 'skobbler',
+                 'opencyclemap', 'osm-transport',
+                 'osm-public-transport', 'osm-bbike', 'osm-bbike-german'
+    )[mapType]
     
-    url <- parse_url("https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task/execute")
-    
-    # define JSON query parameter
-    web_map_param <- list(
-      baseMap = list(
-        baseMapLayers = list(
-          list(url = jsonlite::unbox(glue("https://services.arcgisonline.com/ArcGIS/rest/services/{mapType}/MapServer",
-                                          mapType = mapType)))
-        )
-      ),
-      exportOptions = list(
-        outputSize = c(width, height)
-      ),
-      mapOptions = list(
-        extent = list(
-          spatialReference = list(wkid = jsonlite::unbox(sr_bbox)),
-          xmax = jsonlite::unbox(max(boundingBox$p1$lon, boundingBox$p2$lon)),
-          xmin = jsonlite::unbox(min(boundingBox$p1$lon, boundingBox$p2$lon)),
-          ymax = jsonlite::unbox(max(boundingBox$p1$lat, boundingBox$p2$lat)),
-          ymin = jsonlite::unbox(min(boundingBox$p1$lat, boundingBox$p2$lat))
-        )
-      )
-    )
-    
-    res <- GET(
-      url, 
-      query = list(
-        f = "json",
-        Format = "PNG32",
-        Layout_Template = "MAP_ONLY",
-        Web_Map_as_JSON = jsonlite::toJSON(web_map_param))
-    )
-    
-    if (status_code(res) == 200) {
-      body <- content(res, type = "application/json")
-      message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
-      if (is.null(file)) 
-        file <- tempfile("overlay_img", fileext = ".png")
-      
-      img_res <- GET(body$results[[1]]$value$url)
-      img_bin <- content(img_res, "raw")
-      writeBin(img_bin, file)
-      message(paste("image saved to file:", file))
-    } else {
-      message(res)
-    }
-    invisible(file)
+    print(sprintf('OSM map name: %s', mapType))
   }
   
-  #-------------------------------------------------------------
-  
-  ## Define image width
-  aspectRatio <- abs((boundingBox$p1$lon - boundingBox$p2$lon) / (boundingBox$p1$lat - boundingBox$p2$lat))
-  imageWidth <- ifelse(aspectRatio > 1, majorDim, majorDim*aspectRatio) %>% round()
-  imageHeigth <- ifelse(aspectRatio < 1, majorDim, majorDim/aspectRatio) %>% round()
-  
-  ## fetch overlay image
-  imageFileName <- here::here('figs', 'overlay.png')
-  GetArcGISMapImage(boundingBox, 
-                    mapType = mapType, 
-                    file = imageFileName,
-                    width = imageWidth, height = imageHeigth, 
-                    sr_bbox = 4326
+  # Download map
+  mapObject <-openmap(upperLeft = c(lat = boundingBox$p2$lat, lon = boundingBox$p1$lon),
+                      lowerRight = c(lat = boundingBox$p1$lat, lon = boundingBox$p2$lon), 
+                      zoom = NULL, 
+                      type = mapType,
+                      mergeTiles = TRUE
   )
   
-  return(png::readPNG(imageFileName))
+  # Define image width
+  aspectRatio <- abs((boundingBox$p1$x - boundingBox$p2$x) / (boundingBox$p1$y - boundingBox$p2$y))
+  imageWidth <- ifelse(aspectRatio > 1, majorDim, majorDim*aspectRatio) %>% round()
+  imageHeight <- ifelse(aspectRatio < 1, majorDim, majorDim/aspectRatio) %>% round()
   
+  # fetch overlay image
+  png(filename = imageFileName, width = imageWidth, height = imageHeight)
+  plot(mapObject)
+  dev.off()
+  
+  return(png::readPNG(here::here('figs', 'overlay.png')))
   
 }
+
