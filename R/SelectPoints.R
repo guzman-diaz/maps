148a7c1fb24f5d7832834185a70d0647cef8ef58
good.man <guzman@uniovi.es>
2019-09-08 09:26:10 +0200

    Feature: get marker coordinates

--- R/SelectPoints.R
+++ R/SelectPoints.R
@@ -1,4 +1,8 @@
-SelectPoints <- function(boundingBox, graticuleInterval = 0.1, trackTable = trackTable){
+SelectPoints <- function(boundingBox, 
+                         graticuleInterval = 0.1, 
+                         trackTable = NULL,
+                         markerTable = NULL
+){
   
   
   ui <- fluidPage(
@@ -10,7 +14,8 @@ SelectPoints <- function(boundingBox, graticuleInterval = 0.1, trackTable = trac
     
     output$myMap <- renderLeaflet({ShowOSM(boundingBox = boundingBox, 
                                            graticuleInterval = graticuleInterval, 
-                                           trackTable = trackTable
+                                           trackTable = trackTable,
+                                           markerTable = markerTable
     )})
     
     observeEvent(input$myMap_click, {
@@ -22,11 +27,26 @@ SelectPoints <- function(boundingBox, graticuleInterval = 0.1, trackTable = trac
       
       leafletProxy('myMap') %>%
         addCircles(lng = clickedPoint$lng, lat = clickedPoint$lat, group = 'circles',
-                   weight = 1, radius = 10, color = 'black', fillColor = 'green',
+                   weight = 1, radius = 5, color = 'black', fillColor = 'green',
                    fillOpacity = 0.2, opacity = 1
         )
     })
-  }
+    
+    observeEvent(input$myMap_marker_click, {
+      clickedPoint <- input$myMap_marker_click
+      pointCoords <- c(lon = clickedPoint$lng, lat = clickedPoint$lat)
+      print(pointCoords)
+      
+      pointTable <<- rbind(pointTable, pointCoords)
+      
+      leafletProxy('myMap') %>%
+        addCircles(lng = clickedPoint$lng, lat = clickedPoint$lat, group = 'circles',
+                   weight = 1, radius = 5, color = 'black', fillColor = 'green',
+                   fillOpacity = 0.2, opacity = 1
+        )
+    })
+    
+      }
   
   shinyApp(ui, server)
   
