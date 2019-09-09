6c85975c7ce45e0a81e5138311e0161164c0a1cf
good.man <guzman@uniovi.es>
2019-09-09 14:25:33 +0200

    Feature: select points and store them in global

--- R/SelectPoints.R
+++ R/SelectPoints.R
@@ -1,8 +1,13 @@
 SelectPoints <- function(boundingBox, 
                          graticuleInterval = 0.1, 
-                         trackTable = NULL,
+                         trackList = NULL,
                          markerTable = NULL
 ){
+
+  if (!exists('pointTable', envir = .GlobalEnv)){
+    pointTable <- data.frame(matrix(nrow = 0, ncol = 3)) %>% setNames(c('lon', 'lat', 'id'))
+    assign('pointTable', pointTable, envir = .GlobalEnv)
+  }
   
   
   ui <- fluidPage(
@@ -14,32 +19,16 @@ SelectPoints <- function(boundingBox,
     
     output$myMap <- renderLeaflet({ShowOSM(boundingBox = boundingBox, 
                                            graticuleInterval = graticuleInterval, 
-                                           trackTable = trackTable,
-                                           markerTable = markerTable
+                                           trackList = trackList
     )})
-    
-    ## Observe clicks on any point of the map
-    observeEvent(input$myMap_click, {
-      clickedPoint <- input$myMap_click
-      pointCoords <- c(lon = clickedPoint$lng, lat = clickedPoint$lat)
-      print(pointCoords)
-      
-      pointTable <<- rbind(pointTable, pointCoords)
-      
-      leafletProxy('myMap') %>%
-        addCircles(lng = clickedPoint$lng, lat = clickedPoint$lat, group = 'circles',
-                   weight = 1, radius = 5, color = 'black', fillColor = 'green',
-                   fillOpacity = 0.2, opacity = 1
-        )
-    })
     
     ## Observe clicks on markers
     observeEvent(input$myMap_marker_click, {
       clickedPoint <- input$myMap_marker_click
-      pointCoords <- c(lon = clickedPoint$lng, lat = clickedPoint$lat)
-      print(pointCoords)
-      
-      pointTable <<- rbind(pointTable, pointCoords)
+      pointCoords <- c(lon = clickedPoint$lng, lat = clickedPoint$lat, id = clickedPoint$id)
+
+      pointTable <<- rbind(pointTable, as.data.frame(t(pointCoords)))
+      assign('pointTable', pointTable, envir =  .GlobalEnv)
       
       leafletProxy('myMap') %>%
         addCircles(lng = clickedPoint$lng, lat = clickedPoint$lat, group = 'circles',
@@ -48,7 +37,22 @@ SelectPoints <- function(boundingBox,
         )
     })
     
-      }
+    ## Observe clicks on any point of the map
+    observeEvent(input$myMap_click, {
+      clickedPoint <- input$myMap_click
+      pointCoords <- c(lon = clickedPoint$lng, lat = clickedPoint$lat, id = 'map')
+
+      pointTable <<- rbind(pointTable, as.data.frame(t(pointCoords)))
+      assign('pointTable', pointTable, envir =  .GlobalEnv)
+      
+      leafletProxy('myMap') %>%
+        addCircles(lng = clickedPoint$lng, lat = clickedPoint$lat, group = 'circles',
+                   weight = 1, radius = 5, color = 'black', fillColor = 'green',
+                   fillOpacity = 0.2, opacity = 1
+        )
+    })
+
+  }
   
   shinyApp(ui, server)
   
