da316b6c1ab1feb6dc0480b1ffad883a80060f5d
gut mann <guzman@uniovi.es>
2020-08-28 16:35:55 +0200

    Change variable names

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -37,84 +37,89 @@ Make3DMap <- function(go_boundBox = FALSE,
   # Process TIF
   
   ## Load TIF
-  tifData_raster <- raster::stack(paste(tif_folder, tif_name, sep = '\\'))
+  tif_raster <- raster::stack(paste(tif_folder, tif_name, sep = '\\'))
 
   cat(sprintf(' TIF name: %s\n', tif_name))
-  cat(sprintf(' TIF no. layers: %d\n ', nlayers(tifData_raster)))
-  cat(sprintf('TIF CRS: %s\n', raster::crs(tifData_raster)))
+  cat(sprintf(' TIF no. layers: %d\n ', nlayers(tif_raster)))
+  cat(sprintf('TIF CRS: %s\n', raster::crs(tif_raster)))
 
   ## Mask using shape
   if (go_mask_tif){
     mapShape <- readRDS(paste0(shape_folder, shape_name)) %>% 
-      sp::spTransform(CRSobj = crs(tifData_raster))
+      sp::spTransform(CRSobj = crs(tif_raster))
     
-    tifData_raster <- tifData_raster %>% 
+    tif_raster <- tif_raster %>% 
       raster::crop(extent(mapShape)) %>% 
       raster::mask(mapShape)
   }
   
   ## Crop to bounding box
   if (go_boundBox){
-    tifData_raster <- tifData_raster %>% 
+    tif_raster <- tif_raster %>% 
       raster::crop(c(boundingBox$p1$x, boundingBox$p2$x, boundingBox$p1$y, boundingBox$p2$y))
   }
   
   ## Plot
   if (go_plot_tif){
-    raster::plotRGB(tifData_raster)
+    raster::plotRGB(tif_raster)
   }
   
   
   ## Transform:
     
   ### Transform each band to array
-  tifData_matrix_1 <- rayshader::raster_to_matrix(tifData_raster[[1]])
-  tifData_matrix_2 <- rayshader::raster_to_matrix(tifData_raster[[2]])
-  tifData_matrix_3 <- rayshader::raster_to_matrix(tifData_raster[[3]])
+  tif_matrix_1 <- rayshader::raster_to_matrix(tif_raster[[1]])
+  tif_matrix_2 <- rayshader::raster_to_matrix(tif_raster[[2]])
+  tif_matrix_3 <- rayshader::raster_to_matrix(tif_raster[[3]])
   
   ### Merge band arrays into one tensor:
   #### Initialize
-  tifData_matrix_all <- array(0, dim = c(nrow(tifData_matrix_1), ncol(tifData_matrix_1), 3))
+  tif_matrix <- array(0, dim = c(nrow(tif_matrix_1), ncol(tif_matrix_1), 3))
   
   #### Merge
-  tifData_matrix_all[, , 1] <- tifData_matrix_1/255 
-  tifData_matrix_all[, , 2] <- tifData_matrix_2/255 
-  tifData_matrix_all[, , 3] <- tifData_matrix_3/255
+  tif_matrix[, , 1] <- tif_matrix_1/255 
+  tif_matrix[, , 2] <- tif_matrix_2/255 
+  tif_matrix[, , 3] <- tif_matrix_3/255
   
   #### Transpose to abide by the elevation raster orientation
-  tifData_matrix_all <- aperm(tifData_matrix_all, c(2, 1, 3))
+  tif_matrix <- aperm(tif_matrix, c(2, 1, 3))
 
   
   # Elevation raster
   
   ## Load
-  elevationData_raster <- readRDS(here::here('output', 'rasterObject_Asturias.rds'))
-  cat(sprintf('Elevation CRS: %s\n', raster::crs(elevationData_raster)))
+  ele_raster <- readRDS(here::here('output', 'rasterObject_Asturias.rds'))
+  cat(sprintf('Elevation CRS: %s\n', raster::crs(ele_raster)))
 
   ## Mask
   if (go_mask_elevation){
     mapShape <- readRDS(paste0(shape_folder, shape_name)) %>% 
-      sp::spTransform(CRSobj = crs(elevationData_raster))
+      sp::spTransform(CRSobj = crs(ele_raster))
 
-    elevationData_raster <- elevationData_raster %>% 
+    ele_raster <- ele_raster %>% 
       raster::crop(extent(mapShape)) %>% 
       raster::mask(mapShape)
   }
  
   ## Crop by the bounding box
   if (go_boundBox){
-    elevationData_raster <- elevationData_raster %>% 
+    ele_raster <- ele_raster %>% 
       raster::crop(c(boundingBox$p1$x, boundingBox$p2$x, boundingBox$p1$y, boundingBox$p2$y))
     
   }
   
   ## Transform elevation to matrix
-  elevationData_matrix <- rayshader::raster_to_matrix(elevationData_raster)
+  ele_matrix <- rayshader::raster_to_matrix(ele_raster)
 
   ## Remove NAs (ocean)
-  elevationData_matrix[is.na(elevationData_matrix)] <- 0
+  ele_matrix[is.na(ele_matrix)] <- 0
+  
   
   # Plot
-  plot_3d(tifData_matrix_all, elevationData_matrix, windowsize = c(1100,900), zscale = 50, shadowdepth = -50,
+  plot_3d(tif_matrix, ele_matrix, windowsize = c(1100,900), zscale = 50, shadowdepth = -50,
           zoom=0.5, phi=45,theta=-45,fov=70, background = "#F2E1D0", shadowcolor = "#523E2B")
+  
+  
+  # Output
+  invisible(list(tif_matrix, ele_matrix))
 }
