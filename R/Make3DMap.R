2b4a5fdae6a77aea19bd5692f4ffdc9d088b36bc
gut mann <guzman@uniovi.es>
2020-08-28 16:03:43 +0200

    Feature: crop tif using the bounding box

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -1,7 +1,12 @@
-Make3DMap <- function(go_cropMap = FALSE,
+Make3DMap <- function(go_boundBox = FALSE,
+                      go_mask_elevation = FALSE,
+                      go_mask_tif = FALSE,
                       tif_folder = '\\\\pocpaco\\maps\\overlays\\',
-                      tif_name = 'Asturias_geologico_georef.tif'
-  
+                      tif_name = 'Asturias_1980_georef.tif',
+                      shape_folder = '\\\\pocpaco\\maps\\shapes\\',
+                      shape_name = 'shape_Asturias.rds',
+                      go_plot_tif = TRUE
+                      
 ){
   # Preliminaries
   
@@ -17,26 +22,45 @@ Make3DMap <- function(go_cropMap = FALSE,
   
 
   # Define cropped area manually
-  if (go_cropMap){
+  if (go_boundBox){
     source(here::here('R', 'SelectMapArea.R'))
     source(here::here('R', 'ShowOSM.R'))
     source(here::here('R', 'DefineBoundingBox.R'))
     source(here::here('R', 'TransformCoordinates.R'))
 
-    SelectMapArea()
+    SelectMapArea(environment = environment())
     
     ShowOSM(boundingBox, graticuleInterval = 0.1)
   }
   
-  # Load geoTIF:
+  
+  # Process TIF
+  
+  ## Load TIF
   tifData_raster <- raster::stack(paste(tif_folder, tif_name, sep = '\\'))
+
   cat(sprintf(' TIF name: %s\n', tif_name))
-  
-  # How many bands?
   cat(sprintf(' TIF no. layers: %d\n ', nlayers(tifData_raster)))
-  
-  # Which coordinate reference system?
   cat(sprintf('TIF CRS: %s\n', raster::crs(tifData_raster)))
 
+  ## Mask using shape
+  if (go_mask_tif){
+    mapShape <- readRDS(paste0(shape_folder, shape_name)) %>% 
+      sp::spTransform(CRSobj = crs(tifData_raster))
+    
+    tifData_raster <- tifData_raster %>% 
+      raster::crop(extent(mapShape)) %>% 
+      raster::mask(mapShape)
+  }
+  
+  ## Crop to bounding box
+  if (go_boundBox){
+    tifData_raster <- tifData_raster %>% 
+      raster::crop(c(boundingBox$p1$x, boundingBox$p2$x, boundingBox$p1$y, boundingBox$p2$y))
+  }
+  
+  if (go_plot_tif){
+    raster::plotRGB(tifData_raster)
+  }
   
 }
