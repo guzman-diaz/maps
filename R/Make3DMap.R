fb5d2c8e39876a9aae033eee7d688a85630c2afa
gut mann <guzman@uniovi.es>
2020-08-28 11:11:56 +0200

    Load georeferenced TIF

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -1,4 +1,6 @@
-Make3DMap <- function(go_cropMap = FALSE
+Make3DMap <- function(go_cropMap = FALSE,
+                      tif_folder = '\\\\pocpaco\\maps\\overlays\\',
+                      tif_name = 'Asturias_geologico_georef.tif'
   
 ){
   # Preliminaries
@@ -25,5 +27,16 @@ Make3DMap <- function(go_cropMap = FALSE
     
     ShowOSM(boundingBox, graticuleInterval = 0.1)
   }
+  
+  # Load geoTIF:
+  tifData_raster <- raster::stack(paste(tif_folder, tif_name, sep = '\\'))
+  cat(sprintf(' TIF name: %s\n', tif_name))
+  
+  # How many bands?
+  cat(sprintf(' TIF no. layers: %d\n ', nlayers(tifData_raster)))
+  
+  # Which coordinate reference system?
+  cat(sprintf('TIF CRS: %s\n', raster::crs(tifData_raster)))
+
   
 }
