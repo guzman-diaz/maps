d1b3bd39287834c11a99cfae1680df00cd68ab56
gut mann <guzman@uniovi.es>
2020-09-08 09:50:28 +0200

    Refurbishing.

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -1,29 +1,9 @@
-Make3DMap <- function(mapObjects_lst){
+Make3DMap <- function(mapObject_lst){
   
   # Unlist
-  ele_raster <- mapObject_lst[['ele_raster']]
-  tif_raster <- mapObject_lst[['tif_raster']]
+  ele_matrix <- mapObject_lst[['ele_matrix']]
+  tif_tensor <- mapObject_lst[['tif_tensor']]
   track_lst <- mapObject_lst[['track_lst']]
-  
-  
-  # Transform TIF
-  
-  ## Transform each band to array
-  tif_tensor_1 <- rayshader::raster_to_matrix(tif_raster[[1]])
-  tif_tensor_2 <- rayshader::raster_to_matrix(tif_raster[[2]])
-  tif_tensor_3 <- rayshader::raster_to_matrix(tif_raster[[3]])
-  
-  ## Merge band arrays into one tensor:
-  ### Initialize
-  tif_tensor <- array(0, dim = c(nrow(tif_tensor_1), ncol(tif_tensor_1), 3))
-  
-  ### Merge
-  tif_tensor[, , 1] <- tif_tensor_1/255 
-  tif_tensor[, , 2] <- tif_tensor_2/255 
-  tif_tensor[, , 3] <- tif_tensor_3/255
-  
-  ### Transpose to abide by the elevation raster orientation
-  tif_tensor <- aperm(tif_tensor, c(2, 1, 3))
 
   
   # Plot
@@ -33,7 +13,4 @@ Make3DMap <- function(mapObjects_lst){
           phi = 45, theta = -45, fov=30, 
           background = "#F2E1D0", shadowcolor = "#523E2B"
   )  
-  
-  # Output
-  invisible(list(tif_tensor = tif_tensor, ele_matrix = ele_matrix))
 }
