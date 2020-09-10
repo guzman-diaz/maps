119048a14c6fa97873f09b919a2a498ff822fc25
gut mann <guzman@uniovi.es>
2020-09-10 13:26:19 +0200

    Add path to the 3D surface.

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -10,22 +10,28 @@ Make3DMap <- function(mapObject_lst,
 
   
   # Plot surface
-  plot_3d(tif_tensor, ele_matrix, 
-          windowsize = c(1100,900), 
-          zscale = zscale, zoom = 0.65, 
-          phi = 45, theta = -45, fov=30, 
+  plot_3d(tif_tensor, ele_matrix,
+          windowsize = c(1100,900),
+          zscale = zscale, zoom = 0.65,
+          phi = 45, theta = -45, fov=30,
           background = "#F2E1D0", shadowcolor = "#523E2B"
-  )  
-  
+  )
+
   
   # Overlay tracks
-  lapply(track_lst, function(x)
-    geoviz::add_gps_to_rayshader(ele_raster, 
-                                 lat = x$lat, 
-                                 lon = x$lon, 
-                                 alt = x$elevation, 
-                                 zscale = zscale,
-                                 alpha = 1
+  lapply(track_lst, function(x){
+    ## Tranform track CRS to UTM
+    x[, 1:2] <- TransformCoordinates(x[, 1:2], is.lonLat = T)
+ 
+    render_path(extent = attr(ele_raster, 'extent'), 
+                lat = unlist(x$lat), 
+                long = unlist(x$lon), 
+                altitude = NULL, 
+                # zscale = zscale,
+                heightmap = ele_matrix / zscale,
+                color = 'red',
+                offset = 1
+                
     )
-  )
+  })
 }
