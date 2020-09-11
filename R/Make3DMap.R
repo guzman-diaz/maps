09bf70b502c410d0372062af87747f96b66f4fff
gut mann <guzman@uniovi.es>
2020-09-11 18:12:59 +0200

    Tidy up non-animation code.

--- R/Make3DMap.R
+++ R/Make3DMap.R
@@ -21,34 +21,37 @@ Make3DMap <- function(mapObject_lst,
   }
   
   
-  # Plot surface
-  plot_3d(tif_tensor, ele_matrix,
-          windowsize = c(1100,900),
-          zscale = zscale, zoom = 0.65,
-          phi = 45, theta = -45, fov=30,
-          background = "#F2E1D0", shadowcolor = "#523E2B"
-  )
-  
-  
-  # Overlay tracks
-  # lapply(track_lst[1], function(x){
-  #   ## Tranform track CRS to UTM
-  #   x[, 1:2] <- TransformCoordinates(x[, 1:2], is.lonLat = T)
+  # Plot 3D
+  if (!go_animate){
     
-    #   ## Render path
-    #   render_path(extent = attr(ele_raster, 'extent'), 
-    #               lat = unlist(x$lat), 
-    #               long = unlist(x$lon), 
-    #               altitude = NULL, 
-    #               # zscale = zscale,
-    #               heightmap = ele_matrix / zscale,
-    #               color = 'red',
-    #               offset = 1
-    #               
-    #   )
-    # })
-    # 
-
+    ## Plot surface
+    plot_3d(tif_tensor, ele_matrix,
+            windowsize = c(1100,900),
+            zscale = zscale, zoom = 0.65,
+            phi = 45, theta = theta_ini, fov = 30,
+            background = "#F2E1D0", shadowcolor = "#523E2B"
+    )
+  
+  
+    ## Overlay tracks
+    lapply(track_lst, function(x){
+      ## Tranform track CRS to UTM
+      x[, 1:2] <- TransformCoordinates(x[, 1:2], is.lonLat = T)
+      
+      ## Render path
+      render_path(extent = attr(ele_raster, 'extent'),
+                  lat = unlist(x$lat),
+                  long = unlist(x$lon),
+                  altitude = NULL,
+                  # zscale = zscale,
+                  heightmap = ele_matrix / zscale,
+                  color = 'red',
+                  offset = 1
+                  
+      )
+    })
+  }
+  
   # Animate
   if (go_animate){
     
@@ -81,10 +84,11 @@ Make3DMap <- function(mapObject_lst,
         }
         
         #### Render the path segment
-        render_path(extent = attr(ele_raster, 'extent'), 
-                    lat = unlist(x[1:track_idx[frame_id], 'lat']), 
-                    long = unlist(x[1:track_idx[frame_id], 'lon']), 
-                    altitude = NULL, 
+        render_path(extent = attr(ele_raster, 'extent'),
+                    lat = unlist(x$lat),
+                    long = unlist(x$lon),
+                    altitude = NULL,
+                    zscale = zscale,
                     heightmap = ele_matrix / zscale,
                     color = 'red',
                     offset = 1
